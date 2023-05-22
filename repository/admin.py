973b330b6902fc31472cd2d5885474eb761427f3
Marco <marcolbilli@gmail.com>
2023-05-22 17:56:12 +0200

    Added admin page manipulation

--- repository/admin.py
+++ repository/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Repository)
+admin.site.register(Repo_role)
+admin.site.register(Repo_user)
