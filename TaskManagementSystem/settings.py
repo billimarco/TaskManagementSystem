b37f9e8499d30642f544b6d6e8a253c3ffd1f306
Marco <marcolbilli@gmail.com>
2023-06-04 18:31:14 +0200

    CSRF AUTH added

--- TaskManagementSystem/settings.py
+++ TaskManagementSystem/settings.py
@@ -29,6 +29,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG","") != "False"
 
 ALLOWED_HOSTS = ["djangorailway-production.up.railway.app", "127.0.0.1", "localhost"]
 
+CSRF_TRUSTED_ORIGINS = ["https://tms-billi.up.railway.app"]
+
 
 # Application definition
 
